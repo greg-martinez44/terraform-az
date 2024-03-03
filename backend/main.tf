72889f86a5d9f291b501269a056f560e8febfcb0
Greg Martinez <greg.martinez44@gmail.com>
2024-03-03 13:04:13 -0800

    added storage container

--- backend/main.tf
+++ backend/main.tf
@@ -17,9 +17,20 @@ data "azurerm_resource_group" "azure_tf" {
 }
 
 resource "azurerm_storage_account" "this" {
-  name                     = "tfbackend"
+  name                     = "gmtfbackend"
   resource_group_name      = data.azurerm_resource_group.azure_tf.name
   location                 = data.azurerm_resource_group.azure_tf.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  tags = {
+    Name     = "tfbackend"
+    owner-dl = "greg-martinez44@outlook.com"
+    owner    = "Greg Martinez"
+  }
+}
+
+resource "azurerm_storage_container" "this" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.this.name
+  container_access_type = "private"
 }
